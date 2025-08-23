d901723a6379333518fe9c2ead0771eed5478584
yj <yj@163.com>
2025-08-23 16:50:11 +0800

    fix modelY 2025 CAN error

--- opendbc/car/tesla/carstate.py
+++ opendbc/car/tesla/carstate.py
@@ -48,7 +48,8 @@ class CarState(CarStateBase, TorqueBlendingCarState):
 
     # Brake pedal
     ret.brake = 0
-    ret.brakePressed = cp_party.vl["IBST_status"]["IBST_driverBrakeApply"] == 2
+    # ret.brakePressed = cp_party.vl["IBST_status"]["IBST_driverBrakeApply"] == 2
+    ret.brakePressed = False
 
     # Steering wheel
     epas_status = cp_party.vl["EPAS3S_sysStatus"]
